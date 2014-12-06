5bb142f22ff1c052a72f8e2f9483d16930d0573e
Keaton Grider <keaton.grider@gmail.com>
2014-12-06 16:50:22 -0500

    first commit

---
+++ keaton.js
@@ -0,0 +1,10 @@
+alert('This is from keaton.js Enjoy 	GAME WORLD click ok to continue!') ;
+
+var username = null ;
+if ( username == null )
+{
+  username = prompt( " Please Enter Your Name ");
+	if ( username != "" ) {
+	 alert ( " welcome " + username ) ;
+	} 
+}
